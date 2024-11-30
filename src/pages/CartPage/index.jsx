610bd5f902bf33db990ce84ac950d6eb768a5eb2
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-11-30 17:41:38 +1100

    feat: build empty shopping cart page.
    the empty shopping cart displaying some copy and a routing link back to products page is a separate subcomponent of the CartPage component (called EmptyCart). this is so that it is displayed if the items array prop passed into CartPage parent component is empty. we would otherwise display all of the items. this is one half of issue #8.

--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -1,10 +1,35 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import styles from "./cartPage.module.css";
 
-const CartPage = () => (
+const EmptyCart = () => (
   <>
-    <Header />
-    <h1>This is the Cart page</h1>
+    <p className={styles.paragraphText}>Your cart is currently empty.</p>
+    <Link to="/products" className={styles.link}>
+      Continue shopping
+    </Link>
   </>
 );
+
+const CartPage = ({ items }) => (
+  <>
+    <Header />
+
+    <main>
+      <div className={styles.flex}>
+        <h1 className={styles.heading}>Shopping cart</h1>
+        {items || <EmptyCart />}
+      </div>
+    </main>
+
+    <Footer />
+  </>
+);
+
+CartPage.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default CartPage;
